9566fb5603b7f651f121e4aaecf2c6a1b6489161
edumoritz <eduardo.moritz@hotmail.com>
2020-10-05 00:54:52 -0300

    created project structure

---
+++ backend/src/shared/infra/http/app.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import AppError from '@shared/errors/AppError';
+// import createConnection from '@shared/infra/typeorm';
+import '@shared/container';
+
+// createConnection();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+// app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export default app;
